ca2c0dd87ac747bc4ff7c2c986372cc71e94d973
cflan12 <cflan12@gmail.com>
2015-10-24 01:12:35 -0700

    first commit

---
+++ wp-content/themes/parallax-pro/testimonials-page.php
@@ -0,0 +1,20 @@
+<?php
+ /* Template Name: Testimonials Template */
+ 
+/** Replace the standard loop with our custom loop */
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+add_action( 'genesis_loop', 'wnd_do_custom_loop' );
+  
+function wnd_do_custom_loop() { 
+    global $paged; // current paginated page
+    global $query_args; // grab the current wp_query() args
+    $args = array(
+        'category_name' => 'testimonials', /* shows all posts and child posts from category id */
+        'paged' => $paged, // respect pagination
+    );
+ 
+/* If you want to show posts from a category only and no subcategory posts, use 'category_name' => 'category-slug', instead of 'cat' => 8, for example, 'category_name' => 'articles', */
+    genesis_custom_loop( wp_parse_args($query_args, $args) );
+}
+     
+genesis();
